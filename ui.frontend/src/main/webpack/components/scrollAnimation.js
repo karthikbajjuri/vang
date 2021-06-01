52fae43d3e6e6f6589239f59ea9ec68b330eff7d
kbajjuri <karthik.bajjuri@capgemini.com>
2021-06-01 18:43:07 +0530

    updated teaser and grid

--- ui.frontend/src/main/webpack/components/scrollAnimation.js
+++ ui.frontend/src/main/webpack/components/scrollAnimation.js
@@ -1,4 +1,8 @@
 $(function () {
+  Number.prototype.format = function (n) {
+    var r = new RegExp("\\d(?=(\\d{3})+" + (n > 0 ? "\\." : "$") + ")", "g");
+    return this.toFixed(Math.max(0, Math.floor(n))).replace(r, "$&,");
+  };
   var scrollMagicController = new ScrollMagic.Controller({
     globalSceneOptions: {
       duration: 0,
@@ -7,20 +11,41 @@ $(function () {
       triggerHook: "onEnter",
     },
   });
+  //Fade In Up
   $(".animate-fadeInUp").each(function (index, selector) {
     new ScrollMagic.Scene({ triggerElement: selector })
       .setClassToggle(selector, "js-active")
       .addTo(scrollMagicController);
   });
-
+  //Flip Number
+  $(".flip_numbers").each(function (index, selector) {
+    new ScrollMagic.Scene({ triggerElement: selector })
+      .on("enter", function () {
+        $(".fadeIn").hide();
+        $(".fadeOut").show();
+        setTimeout(function () {
+          $("." + selector.classList[0]).addClass("flip");
+        }, 200);
+        setTimeout(function () {
+          $(".fadeOut").fadeOut(function () {
+            $(".fadeIn").fadeIn();
+          });
+        }, 1200);
+      })
+      .on("leave", function () {
+        $("." + selector.classList[0]).removeClass("flip");
+      })
+      .addTo(scrollMagicController);
+  });
+  //Number Increment
   new ScrollMagic.Scene({
     triggerElement: ".counter_number",
     duration: $(".counter_number").height(),
   })
     .on("enter", function () {
       $(".number").each(function () {
-        var oldtext = $(this).text();
-        $(this).text(oldtext.replace(/,/g, ""));
+        var oldtext = $(this).data("count");
+        $(this).text(oldtext);
         $(this)
           .prop("counter", 0)
           .animate(
@@ -38,9 +63,4 @@ $(function () {
       });
     })
     .addTo(scrollMagicController);
-
-  Number.prototype.format = function (n) {
-    var r = new RegExp("\\d(?=(\\d{3})+" + (n > 0 ? "\\." : "$") + ")", "g");
-    return this.toFixed(Math.max(0, Math.floor(n))).replace(r, "$&,");
-  };
 });
