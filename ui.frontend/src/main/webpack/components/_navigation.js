8bc07c45c0d66b7a045bf3f6ccb45123ba262476
krishna sahithi <krishna-sahithi.akula@capgemini.com>
2021-06-07 13:08:54 +0530

    Navigation js changes

--- ui.frontend/src/main/webpack/components/_navigation.js
+++ ui.frontend/src/main/webpack/components/_navigation.js
@@ -87,7 +87,7 @@ $(function () {
       }
     });
   }
-  $('a[href*=\\#]').on('click', function(e){
+  $("a[href^='#']").on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
             scrollTop : $(this.hash).offset().top
